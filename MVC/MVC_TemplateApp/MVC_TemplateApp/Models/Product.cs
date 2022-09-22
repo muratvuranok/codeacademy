f35df23040325733eaff38547550451c64828821
murat vuranok <murat.vuranok@247pro.com>
2022-09-22 09:11:23 +0300

    template

---
+++ MVC/MVC_TemplateApp/MVC_TemplateApp/Models/Product.cs
@@ -0,0 +1,27 @@
+﻿namespace MVC_TemplateApp.Models;
+
+public class Product
+{
+    public Product()
+    {
+        this.ProductPhotos = new HashSet<ProductPhoto>();
+    }
+
+    public Guid Id { get; set; }
+    public string Name { get; set; } = null!;
+    public decimal Price { get; set; }
+    public double Rate { get; set; }
+    public virtual ICollection<ProductPhoto> ProductPhotos { get; set; }
+}
+
+
+public class ProductPhoto
+{
+    public Guid Id { get; set; }
+    public Guid ProductId { get; set; }
+    public Product Product { get; set; } = null!;
+    public string Url { get; set; } = null!;
+}
+
+ 
+ 
